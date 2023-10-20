85d8264ba572ce7f0e53f0fbda1d01b35827d8c1
Vamshi <90742188+mintuguptha@users.noreply.github.com>
2023-10-20 17:16:22 +0530

    Update application.py

--- application.py
+++ application.py
@@ -5,10 +5,6 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World 2 to test docker vesion'
-
-@app.route('/admin')
-def hello_world():
-    return 'Hello Admin'
  
 if __name__ == '__main__': 
     app.run(debug=True)
